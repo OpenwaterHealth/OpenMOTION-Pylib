bdbbf78548573b506d6a84c97cf26a8335fe6f55
George Vigelette <george@openwater.cc>
2025-08-25 14:33:37 -0400

    usb driver installer setup package

---
+++ winusb-driver/driver_install.cmd
@@ -0,0 +1,14 @@
+@echo off
+setlocal enableextensions
+set DIR=%~dp0
+
+rem Install the public certificate into Trusted Publishers and Root
+"%SystemRoot%\System32\certutil.exe" -addstore -f TrustedPublisher "%DIR%OpenMotion_signing_cert.cer" || exit /b 1
+"%SystemRoot%\System32\certutil.exe" -addstore -f Root             "%DIR%OpenMotion_signing_cert.cer" || exit /b 1
+
+rem Install all three driver packages
+"%SystemRoot%\System32\pnputil.exe" /add-driver "%DIR%COMMS_HISTO_IMU(HS)_(Interface_0).inf" /install || exit /b 1
+"%SystemRoot%\System32\pnputil.exe" /add-driver "%DIR%COMMS_HISTO_IMU(HS)_(Interface_1).inf" /install || exit /b 1
+"%SystemRoot%\System32\pnputil.exe" /add-driver "%DIR%COMMS_HISTO_IMU(HS)_(Interface_2).inf" /install || exit /b 1
+
+exit /b 0
