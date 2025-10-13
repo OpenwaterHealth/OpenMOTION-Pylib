6c36a5e6e645286e55706a48a9ba1124787d3b3f
boringethan <48163023+boringethan@users.noreply.github.com>
2025-10-13 14:56:59 -0700

    Fix test_camera.bat

--- test_camera.bat
+++ test_camera.bat
@@ -2,13 +2,20 @@ del histogram.bin
 del histogram.csv
 
 :: check if the current directory is in PYTHONPATH
-@echo off "%PYTHONPATH%"=="" (
+if "%PYTHONPATH%"=="" (
     echo PYTHONPATH is not set, setting it to the current directory.
-    set PYTHONPATH=%cd%;%PYTHONPATH%
+    set PYTHONPATH=%cd%
 ) else (
     echo PYTHONPATH is currently set to: %PYTHONPATH%
+    echo %PYTHONPATH% | findstr /i "%cd%" >nul
+    if errorlevel 1 (
+        echo Current directory not in PYTHONPATH, adding it.
+        set PYTHONPATH=%cd%;%PYTHONPATH%
+    ) else (
+        echo Current directory is already in PYTHONPATH.
+    )
 )
 
-python scripts\flash_sensors.py 0F
+python scripts\flash_sensors.py --camera-mask 0x01
 
-python scripts\camera_tester.py 0F
+python scripts\camera_tester.py 01
